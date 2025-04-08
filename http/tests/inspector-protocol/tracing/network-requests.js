6b38ebed5b9184af24575d35b1b4775e5750f894
hlzhangaj <zhanghanlin16@h-partners.com>
2025-04-08 14:17:10 +0800

    add code
    Signed-off-by:hlzhangaj <zhanghanlin16@h-partners.com>

--- http/tests/inspector-protocol/tracing/network-requests.js
+++ http/tests/inspector-protocol/tracing/network-requests.js
@@ -1,4 +1,4 @@
-(async function(testRunner) {
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
   const {session, dp} = await testRunner.startBlank(
       'Tests the data of Network request lifecycle trace events');
 
@@ -7,49 +7,75 @@
   const tracingHelper = new TracingHelper(testRunner, session);
 
   await dp.Page.enable();
+  await dp.Network.enable();
+
   await tracingHelper.startTracing('devtools.timeline');
-  dp.Page.navigate({
-    url: 'http://127.0.0.1:8000/inspector-protocol/resources/basic.html'
+
+  dp.Page.navigate(
+      {url: 'http://127.0.0.1:8000/inspector-protocol/resources/basic.html'});
+
+  // Get the ID of the request for the HTML page
+  const htmlRequest = await dp.Network.onceRequestWillBeSent(e => {
+    return e.params.request.url.includes('basic.html');
   });
 
-  // Wait for the DOM to be interactive.
-  await dp.Page.onceLoadEventFired();
+  // Bind a listener for when the HTML request is completely finished.
+  // Note that we don't await it yet as we want to wait for responses from
+  // our 4 expected requests first.
+  const htmlRequestDone = dp.Network.onceLoadingFinished(e => {
+    return e.params.requestId === htmlRequest.params.requestId;
+  });
+
+  await new Promise(resolve => {
+    let count = 0;
+    dp.Network.onResponseReceived(() => {
+        ++count;
+      // We expect 4 requests:
+      // - basic.html
+      // - empty.js
+      // - square.png
+      // - style.css
+        if (count === 4) resolve();
+    });
+  });
+
+  // Ensure the HTML request is completely finished before any assertions
+  await htmlRequestDone;
 
   const timelineEvents = await tracingHelper.stopTracing(/devtools.timeline/);
 
-  // Find and test all events of the lifecycle of the request for the
-  // HTML document.
-  const resourceSendRequest = timelineEvents.find(
-      event => event.name === 'ResourceSendRequest' &&
-          event.args.data.url.includes('basic.html'));
-  const resourceWillSendRequest = timelineEvents.find(
-      event => eventBelongsToDocumentRequest(event, 'ResourceWillSendRequest'));
-  const resourceReceiveResponse = timelineEvents.find(
-      event => eventBelongsToDocumentRequest(event, 'ResourceReceiveResponse'));
-  const resourceReceivedData = timelineEvents.find(
-      event => eventBelongsToDocumentRequest(event, 'ResourceReceivedData'));
-  const resourceFinish = timelineEvents.find(
-      event => eventBelongsToDocumentRequest(event, 'ResourceFinish'));
+  const eventNames = new Set(['ResourceSendRequest', 'ResourceWillSendRequest', 'ResourceReceiveResponse', 'ResourceReceivedData', 'ResourceFinish']);
+  const eventsByRequestId = new Map();
+  const requestIdToUrl = new Map();
 
-  testRunner.log('Got ResourceWillSendRequest event:');
-  tracingHelper.logEventShape(resourceWillSendRequest)
+  for (const event of timelineEvents) {
+    if (!eventNames.has(event.name)) {
+        continue;
+    }
+    if (!event.args.data.requestId) continue;
+    const events = eventsByRequestId.get(event.args.data.requestId) || [];
+    events.push(event);
+    eventsByRequestId.set(event.args.data.requestId, events);
 
-  testRunner.log('Got ResourceSendRequest event:');
-  tracingHelper.logEventShape(resourceSendRequest)
+    if (event.args.data.url) {
+        requestIdToUrl.set(event.args.data.requestId, event.args.data.url);
+    }
+  }
 
-  testRunner.log('Got ResourceReceiveResponse event:');
-  tracingHelper.logEventShape(resourceReceiveResponse)
-
-  testRunner.log('Got ResourceReceivedData event:');
-  tracingHelper.logEventShape(resourceReceivedData)
-
-  testRunner.log('Got ResourceFinish event:');
-  tracingHelper.logEventShape(resourceFinish)
+  const orderedKeys = Array.from(requestIdToUrl.entries())
+    .sort((a, b) => a[1].localeCompare(b[1]))
+    .map(r => r[0]);
+  for (const key of orderedKeys) {
+    testRunner.log(`\nTrace events for URL: ${requestIdToUrl.get(key)}:`);
+    const events = eventsByRequestId.get(key);
+    for (const event of events) {
+      tracingHelper.logEventShape(event, ['headers'], ['name', 'resourceType', 'isLinkPreload', 'fetchPriorityHint', 'fetchType', 'protocol']);
+      if(event.name === 'ResourceReceiveResponse') {
+        const headersCount = event.args.data.headers.length ?? 0;
+        testRunner.log(`${event.name} has headers: ${headersCount > 0}\n`);
+      }
+    }
+  }
 
   testRunner.completeTest();
-
-  function eventBelongsToDocumentRequest(event, expectedName) {
-    return event.name === expectedName &&
-        event.args.data.requestId === resourceSendRequest.args.data.requestId;
-  }
 });
--- http/tests/inspector-protocol/tracing/user-marks.js
+++ http/tests/inspector-protocol/tracing/user-marks.js
@@ -1,4 +1,4 @@
-(async function(testRunner) {
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
   const {session, dp} =
       await testRunner.startBlank('Tests the data of user marks trace events');
 
@@ -29,7 +29,7 @@
   const consoleTimeEvents =
       allEvents.filter(event => event.name === 'console time');
 
-  const performanceMark = tracingHelper.findEvent('startMark', Phase.MARK);
+  const performanceMark = tracingHelper.findEvent('startMark', Phase.INSTANT);
   const userTimings = allEvents.filter(event => event.name === 'user timing');
 
   const timerRemove = tracingHelper.findEvent('TimerRemove', Phase.INSTANT);
@@ -76,11 +76,11 @@
   testRunner.log('Got a performance mark event:');
   tracingHelper.logEventShape(performanceMark);
 
-  testRunner.log('Got user timings events:');
+  testRunner.log('Got performance measure event:');
   tracingHelper.logEventShape(userTimings[0]);
   testRunner.log(`Phase of begin event: ${userTimings[0].ph}`);
   testRunner.log(`Phase of end event: ${userTimings[1].ph}`);
-  if (userTimings[0].id === userTimings[1].id) {
+  if (userTimings[0].id2.local === userTimings[1].id2.local) {
     testRunner.log('user timing event ids are equal.');
   }
 
